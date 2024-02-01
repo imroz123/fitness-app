412c2363bdb5437fe84612f3a8d8ae5b2feb8b84
imroz.dawar@gmail.com <imroz.dawar@gmail.com>
2024-02-01 15:42:15 +0530

    authorisation implementation

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fitnessapp3637cf1f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
